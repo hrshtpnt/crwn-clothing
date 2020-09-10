2026d87082b7610ff5fdbba28873dd3d89f5802c
harshit <harshit.pant@hpe.com>
2020-09-10 11:51:23 +0530

    fixed issues with the sign in with google

--- src/components/signUp/sign-up.component.jsx
+++ src/components/signUp/sign-up.component.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import { firestore } from "../../firebase/firebase.utils";
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+  printLogs = async () => {
+    const test = await firestore.collection(
+      "users/tUNsesHOtkO9cCVmo7zn/cartItem"
+    );
+    console.log(test);
+  };
+  componentDidMount() {
+    this.printLogs();
   }
   render() {
     return <div>Sign up component</div>;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -23,7 +23,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_acount" });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
